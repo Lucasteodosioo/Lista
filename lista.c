28352f86925de980e5fe30133b6614ce706cae3c
Lucas Teodosio <161146252+Lucasteodosioo@users.noreply.github.com>
2025-05-07 19:13:17 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -71,6 +71,57 @@ void inserir_meio(No **lista, int ant){
         printf("Erro ao alocar memoria.\n");
 }
 
+void inserir_ordenado(No **lista){
+    No *novo = malloc(sizeof(No));
+    No *aux;
+
+    if (novo){
+        novo->num = ler_num();
+        if (*lista == NULL){
+            novo->prox = NULL;
+            *lista = novo;
+        }
+        else if (novo->num < (*lista)->num){
+            novo->prox = *lista;
+            *lista = novo;
+        }
+        else{
+            aux = *lista;
+            while (aux->prox && novo->num > aux->prox->num)
+                aux = aux->prox;
+            novo->prox = aux->prox;
+            aux->prox = novo;
+        }   
+    }
+    else
+        printf("Erro ao alocar memoria.\n");
+}
+
+No* pop(No **lista, int num){
+    No *remover = NULL;
+    No *aux;
+
+    if (*lista){
+        if ((*lista)->num == num){
+            remover = *lista;
+            *lista = remover->prox;
+        }
+        else{
+            aux = *lista;
+            while (aux->prox && aux->prox->num != num){
+                aux = aux->prox;
+            }
+            if (aux->prox){
+                remover = aux->prox;
+                aux->prox = remover->prox;
+
+            }
+        }
+    }
+
+    return remover;
+}
+
 void imprimir(No *no){
     printf("Lista: \n");
     while (no){
@@ -83,10 +134,12 @@ void imprimir(No *no){
 int main(){
     int opcao;
     int ref;
+    int remover;
     No *l = NULL;
+    No *remove;
 
     while(1){
-        printf("1 inserir no comeco - 2 inserir no fim - 3 inserir no meio - 4 imprimir - 0 sair\n");
+        printf("1 inserir no comeco - 2 inserir no fim - 3 inserir no meio - 4 inserir ordenado - 5 remover - 6 imprimir - 0 sair\n");
         scanf("%d", &opcao);
 
         switch (opcao)
@@ -102,7 +155,15 @@ int main(){
             scanf("%d", &ref);
             inserir_meio(&l, ref);
             break;
-        case 4: 
+        case 4:
+            inserir_ordenado(&l);
+            break;
+        case 5: 
+            printf("Digite o numero que voce deseja remover: \n");
+            scanf("%d", &remover);
+            remove = pop(&l, remover);
+            break;
+        case 6:
             imprimir(l);
             break;
         case 0:
